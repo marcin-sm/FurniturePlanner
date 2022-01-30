31fcc6d27fca7fd2c10068c449475313d3e6dc81
marcin-sm <marcinsmietanka@gmail.com>
2022-01-30 22:56:45 +0100

    renaming

--- Labels/Labels.py
+++ Labels/Labels.py
@@ -2,6 +2,7 @@ from sre_constants import IN
 from turtle import width
 from blabel import LabelWriter
 
+
 def Prop (width,height,ret):
     Xmax = 23.5
     Ymax = 15
@@ -40,14 +41,14 @@ def Prop (width,height,ret):
 
 
 
-label_writer = LabelWriter("BarCodes/item_template.html", default_stylesheets=("BarCodes/style.css",))
+label_writer = LabelWriter("Labels/item_template.html", default_stylesheets=("Labels/style.css",))
 records = [
     dict(sample_id="4628935901", sample_name="4 628 935901",Lwidth= "720", Lheight="510" ,plate_width=str(Prop(510,720,"W")), plate_height=str(Prop(510,720,"H")), border_top="0.7", border_bottom="0.7", border_left="0.1", border_right="0.1", space=str(Prop(510,720,"S")), onStock=True, number="1" ),
-    dict(sample_id="4628935901", sample_name="4 628 935901",Lwidth= "730", Lheight="600" ,plate_width=str(Prop(600,730,"W")), plate_height=str(Prop(600,730,"H")), border_top="0.7", border_bottom="0.7", border_left="0.1", border_right="0.7", space=str(Prop(600,730,"S")), onStock=False, number="23" ),
-    dict(sample_id="4628935901", sample_name="4 628 935901",Lwidth= "2070", Lheight="510" ,plate_width=str(Prop(710,2070,"W")), plate_height=str(Prop(710,2070,"H")), border_top="0.7", border_bottom="0.7", border_left="0.7", border_right="0.7", space=str(Prop(710,2070,"S")), onStock=True, number="9" ),
+    dict(sample_id="4628935902", sample_name="4 628 935902",Lwidth= "730", Lheight="600" ,plate_width=str(Prop(600,730,"W")), plate_height=str(Prop(600,730,"H")), border_top="0.7", border_bottom="0.7", border_left="0.1", border_right="0.7", space=str(Prop(600,730,"S")), onStock=False, number="23" ),
+    dict(sample_id="4628935903", sample_name="4 628 935903",Lwidth= "2070", Lheight="510" ,plate_width=str(Prop(710,2070,"W")), plate_height=str(Prop(710,2070,"H")), border_top="0.7", border_bottom="0.7", border_left="0.7", border_right="0.7", space=str(Prop(710,2070,"S")), onStock=True, number="9" ),
     dict(sample_id="4859276283", sample_name="4 859 276283",plate_width="12", plate_height="10", border_top="0.1", border_bottom="0.1", border_left="0.7", border_right="0.1", space="4", onStock=False ),
     dict(sample_id="4783926189", sample_name="4 783 926189",plate_width="22", plate_height="11", border_top="0.1", border_bottom="0.7", border_left="0.7", border_right="0.7", space="1", onStock=True ),
     dict(sample_id="4734654324", sample_name="4 734 654324",plate_width="11", plate_height="11", border_top="0.7", border_bottom="0.1", border_left="0.7", border_right="0.1", space="3", onStock=False ),
 ]
 
-label_writer.write_labels(records, target="BarCodes/qrcode_and_date.pdf")
+label_writer.write_labels(records, target="Labels/labels.pdf")
