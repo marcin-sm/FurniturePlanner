2e3d3636a557f837ee6d5b9aa822d07a6d4b7d1c
marcin-sm <marcinsmietanka@gmail.com>
2022-02-10 22:16:30 +0100

    adding qr code scanner to flask web app

--- app_web.py
+++ app_web.py
@@ -1,7 +1,9 @@
 #bash-> export FLASK_APP=app_web.py
+#bash-> export FLASK_ENV=development
 #bash-> python -m flask run --host=0.0.0.0   
 
 from flask import Flask
+from flask import render_template
 from markupsafe import escape
 from FurniturePlanner import *
 app = Flask(__name__)
@@ -18,6 +20,14 @@ def show_post(post_id):
     info = Prod.OperationsProgress (prt = False)
     #return f'Post {post_id}'
     return str(info)
+
+#@app.route('/code/<int:code>')
+#def scan(code=None):
+#    return render_template('print.html', code=str(code))
+
+@app.route('/')
+def home():
+    return render_template('print.html')
 
 if __name__ == "__main__":
     app.run()
